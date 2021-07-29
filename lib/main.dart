4add5e85b853ac7735199bb361d23c0a841c4d9a
AlixBou <alixb@bam.tech>
2021-07-29 17:24:50 +0200

    improve-timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,7 +30,6 @@ enum breath_status { expire, inspire, hold }
 
 class MyHomePageState extends State<MyHomePage> {
   breath_status breathStatus = breath_status.inspire;
-
   bool visible = true;
 
   @override
@@ -40,7 +41,7 @@ class MyHomePageState extends State<MyHomePage> {
                 gradient: LinearGradient(
               begin: Alignment(0.5, 0),
               end: Alignment(0.5, 1),
-              stops: [0, 0.9],
+              stops: [0, 0.8],
               colors: [
                 Color.fromRGBO(255, 142, 142, 1),
                 Color.fromRGBO(255, 247, 220, 1),
@@ -66,17 +67,20 @@ class MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               AnimatedPositioned(
-                top: breathStatus == breath_status.expire ? 200 : 400,
+                top: breathStatus == breath_status.expire ? 200 : 360,
                 duration: const Duration(seconds: 5),
                 curve: Curves.fastOutSlowIn,
                 child: GestureDetector(
                   onTap: () {
-                    setState(() {
-                      if (breathStatus == breath_status.expire) {
-                        breathStatus = breath_status.inspire;
-                      } else {
-                        breathStatus = breath_status.expire;
-                      }
+                    breathStatus = breath_status.expire;
+                    Timer.periodic(Duration(seconds: 5), (timer) {
+                      setState(() {
+                        if (breathStatus == breath_status.expire) {
+                          breathStatus = breath_status.inspire;
+                        } else {
+                          breathStatus = breath_status.expire;
+                        }
+                      });
                     });
                   },
                   child: Container(
@@ -87,27 +91,29 @@ class MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               AnimatedPositioned(
-                bottom: breathStatus == breath_status.expire ? -10 : 800,
-                duration: const Duration(seconds: 3),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AnimatedOpacity(
-                        opacity: visible ? 1.0 : 0.0,
-                        duration: const Duration(milliseconds: 500),
-                        child: Text(
-                          breathStatus == breath_status.expire
-                              ? 'Expirez'
-                              : 'Inspirez',
-                          style: TextStyle(
-                            fontSize: 48.0,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromRGBO(255, 200, 200, 1),
-                          ),
+                bottom: breathStatus == breath_status.expire ? 20 : 600,
+                duration: const Duration(seconds: 5),
+                child: AnimatedOpacity(
+                  opacity: breathStatus == breath_status.expire ? 1.0 : 0.0,
+                  duration: const Duration(seconds: 3),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 500),
+                      child: Text(
+                        breathStatus == breath_status.expire
+                            ? 'Expirez'
+                            : 'Inspirez',
+                        style: TextStyle(
+                          fontSize: 48.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(255, 200, 200, 1),
                         ),
-                      )
-                    ]),
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ]))),
       ),
